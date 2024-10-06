3d6f695eaf1e610c9f9b191e5a01df9945e4dd30
Trung <wowholyshit07@gmail.com>
2024-10-06 18:34:44 +0700

    Project ggmap

---
+++ app/src/main/java/com/example/project_googlemaps/MainActivity.java
@@ -0,0 +1,165 @@
+package com.example.project_googlemaps;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private final int FINE_PERMISSION_CODE = 1;
+    private GoogleMap myMap;
+    private SearchView mapSearchView;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapSearchView = findViewById(R.id.mapSearch);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+
+        mapSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                String location = mapSearchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (location != null){
+                    Geocoder geocoder = new Geocoder(MainActivity.this);
+
+                    try{
+                        addressList = geocoder.getFromLocationName(location,1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+                    myMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+    }
+
+    private void getLastLocation(){
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null){
+                    currentLocation = location;
+
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
+                    mapFragment.getMapAsync(MainActivity.this);
+                }
+            }
+        });
+    }
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        myMap = googleMap;
+        LatLng myLocation = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        myMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+
+        myMap.getUiSettings().setZoomControlsEnabled(true);
+        myMap.getUiSettings().setCompassEnabled(true);
+
+        myMap.getUiSettings().setZoomGesturesEnabled(false);
+        myMap.getUiSettings().setScrollGesturesEnabled(false);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == FINE_PERMISSION_CODE){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLastLocation();
+            }else {
+                Toast.makeText(this, "Please allow permission to get current location", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if(id == R.id.mapNone) {
+            myMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+        }
+
+        if(id == R.id.mapNormal) {
+            myMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+
+        if(id == R.id.mapSatellite) {
+            myMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
+
+        if(id == R.id.mapHybrid) {
+            myMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        }
+
+        if(id == R.id.mapTerrain) {
+            myMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
